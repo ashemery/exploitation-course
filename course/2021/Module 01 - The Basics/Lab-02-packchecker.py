762a458c07dc552e8734ff5c29427b06b417b78c
binary <ali@ashemery.com>
2021-07-01 00:17:45 -0400

    Adding the OSRE 2021 Course

---
+++ course/2021/Module 01 - The Basics/Lab-02-packchecker.py
@@ -0,0 +1,24 @@
+import pefile
+import math
+import sys
+
+def H(data):
+    if not data:
+        return 0
+    entropy = 0
+    for x in range(256):
+        p_x = float(data.count(chr(x)))/len(data)
+        if p_x > 0:
+            entropy += - p_x*math.log(p_x, 2)
+    return entropy
+
+if len(sys.argv) > 1:
+    fname = sys.argv[1]
+    pe = pefile.PE(fname, fast_load=True)
+    for section in pe.sections:
+        print section.Name, H(section.get_data())
+else:
+    print "usage: \npackchecker.py file.exe"
+
+
+
